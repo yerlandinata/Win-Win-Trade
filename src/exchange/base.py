8427e0e1d0d79662730565a62f3f2288f9b74b43
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-01-06 16:48:36 +0700

    add base exchange

---
+++ src/exchange/base.py
@@ -0,0 +1,16 @@
+class ExchangeAccount:
+
+    def get_balance(self):
+        pass
+
+    def get_order(id=None):
+        pass
+
+    def is_order_fulfilled(id):
+        pass
+
+    def place_buy_order(currency_pair, amount):
+        pass
+
+    def place_sell_order(currency_pair, amount):
+        pass
