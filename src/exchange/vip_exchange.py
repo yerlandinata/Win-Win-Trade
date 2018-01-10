e66abc264d4d61228439bea28cc3ddf5c61f007e
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-01-10 21:55:50 +0700

    update vip_exchange use valid currency list

--- src/exchange/vip_exchange.py
+++ src/exchange/vip_exchange.py
@@ -5,12 +5,22 @@ import json
 from collections import OrderedDict
 from urllib.parse import urlencode
 import requests
+from src.valid_pairs import *
 from src.trader import VipOrder
 from . import ExchangeAccount, ExchangeOperationFailedError
 
 class VipExchangeAccount(ExchangeAccount):
 
     BASE_URL = 'https://vip.bitcoin.co.id/tapi'
+    PAIRS = {
+        BTCIDR: 'btc_idr',
+        ETHIDR: 'eth_idr',
+        BCHIDR: 'bch_idr',
+        BTGIDR: 'btg_idr',
+        XLMIDR: 'xlm_idr',
+        XRPIDR: 'xrp_idr',
+        NXTIDR: 'nxt_idr'
+    }
 
     def __init__(self, api_key, secret):
         self.__api_key = api_key
@@ -29,10 +39,12 @@ class VipExchangeAccount(ExchangeAccount):
         Arguments:
         order_id, currency_pair
         '''
+        if kwargs['currency_pair'] not in VipExchangeAccount.PAIRS:
+            raise RuntimeError('Invalid currency pair: ' + kwargs['currency_pair'])
         payload = OrderedDict([
             ('nonce', str(int(datetime.now().timestamp()))),
             ('method', 'getOrder'),
-            ('pair', kwargs['currency_pair']),
+            ('pair', VipExchangeAccount.PAIRS[kwargs['currency_pair']]),
             ('order_id', kwargs['order_id'])
         ])
         res = self.post_request(payload)
@@ -45,7 +57,7 @@ class VipExchangeAccount(ExchangeAccount):
             if 'remain' in key:
                 remain_key = key
                 break
-        return VipOrder(self, str(kwargs['order_id']), kwargs['currency_pair'], res['return']['order']['type'],
+        return VipOrder(self, str(kwargs['order_id']), VipExchangeAccount.PAIRS[kwargs['currency_pair']], res['return']['order']['type'],
                         float(res['return']['order']['price']), int(res['return']['order']['submit_time']),
                         float(res['return']['order'][remain_key]), finish_time=int(res['return']['order']['finish_time']) if int(res['return']['order']['finish_time']) else None)
 
@@ -54,7 +66,9 @@ class VipExchangeAccount(ExchangeAccount):
         Arguments:
         currency_pair, price, amount
         '''
-        return self.__place_order(kwargs['currency_pair'], kwargs['currency_pair'].split('_')[0],
+        if kwargs['currency_pair'] not in VipExchangeAccount.PAIRS:
+            raise RuntimeError('Invalid currency pair: ' + kwargs['currency_pair'])
+        return self.__place_order(kwargs['currency_pair'], VipExchangeAccount.PAIRS[kwargs['currency_pair']].split('_')[0],
                                   'buy', kwargs['price'], kwargs['amount'])
 
     def place_sell_order(self, **kwargs):
@@ -62,14 +76,16 @@ class VipExchangeAccount(ExchangeAccount):
         Arguments:
         currency_pair, price, amount
         '''
-        return self.__place_order(kwargs['currency_pair'], kwargs['currency_pair'].split('_')[1],
+        if kwargs['currency_pair'] not in VipExchangeAccount.PAIRS:
+            raise RuntimeError('Invalid currency pair: ' + kwargs['currency_pair'])
+        return self.__place_order(kwargs['currency_pair'], VipExchangeAccount.PAIRS[kwargs['currency_pair']].split('_')[1],
                                   'sell', kwargs['price'], kwargs['amount'])
 
     def __place_order(self, currency_pair, currency_ask, order_type, price, amount):
         payload = OrderedDict([
             ('nonce', str(int(datetime.now().timestamp()))),
             ('method', 'trade'),
-            ('pair', currency_pair),
+            ('pair', VipExchangeAccount.PAIRS[currency_pair]),
             ('type', order_type),
             ('price', str(price)),
             (currency_ask, str(amount))
--- tests/exchange/vip_exchange_test.py
+++ tests/exchange/vip_exchange_test.py
@@ -4,6 +4,7 @@ from itertools import tee
 import requests
 import pytest
 from unittest.mock import call
+from src.valid_pairs import *
 from src.exchange import ExchangeOperationFailedError, VipExchangeAccount
 from src.trader import VipOrder
 
@@ -64,10 +65,10 @@ def test_get_balance(exchange, req_mock):
     assert req_mock.call_count == len(currencies)
 
 def test_get_order_with_id(exchange, req_mock):
-    pair = 'ltc_idr'
+    pair = XLMIDR
     order_id = '94425'
-    req_mock.return_value.content = '{"success": 1,"return": {"order": {"order_id": "94425","price": "0.00810000","type": "sell","order_ltc": "1.00000000","remain_ltc": "0.53000000","submit_time": "1497657065","finish_time": "0","status": "open"}}}'
-    expected = VipOrder(exchange, order_id, 'ltc_idr', 'sell', 0.0081, 1497657065, 0.53)
+    req_mock.return_value.content = '{"success": 1,"return": {"order": {"order_id": "94425","price": "0.00810000","type": "sell","order_xlm": "1.00000000","remain_xlm": "0.53000000","submit_time": "1497657065","finish_time": "0","status": "open"}}}'
+    expected = VipOrder(exchange, order_id, 'xlm_idr', 'sell', 0.0081, 1497657065, 0.53)
     actual_test = exchange.get_order(order_id=order_id, currency_pair=pair)
     assert actual_test == expected
     args, kwargs = req_mock.call_args
@@ -75,12 +76,20 @@ def test_get_order_with_id(exchange, req_mock):
     assert kwargs['data'] == OrderedDict([
         ('nonce', str(int(datetime.now().timestamp()))),
         ('method', 'getOrder'),
-        ('pair', pair),
+        ('pair', VipExchangeAccount.PAIRS[XLMIDR]),
         ('order_id', order_id)
     ])
 
+def test_get_order_with_wrong_currency_pair(exchange, req_mock):
+    pair = 'usdidr'
+    order_id = '94425'
+    req_mock.return_value.content = '{"success": 0}'
+    with pytest.raises(RuntimeError) as excinfo:
+        exchange.get_order(order_id=order_id, currency_pair=pair)
+    assert 'Invalid currency pair: ' + pair in str(excinfo.value)
+
 def test_get_order_with_id_fail(exchange, req_mock):
-    pair = 'ltc_idr'
+    pair = XLMIDR
     order_id = '94425'
     req_mock.return_value.content = '{"success": 0, "error": "some error"}'
     with pytest.raises(ExchangeOperationFailedError) as excinfo:
@@ -88,7 +97,7 @@ def test_get_order_with_id_fail(exchange, req_mock):
     assert str(excinfo.value) == 'some error'
 
 def test_place_buy_order(exchange, req_mock_place_order):
-    pair = 'btc_idr'
+    pair = BTCIDR
     order_id = '11560'
     price = 10000
     amount = 1.2
@@ -96,14 +105,14 @@ def test_place_buy_order(exchange, req_mock_place_order):
     req, trade, order = req_mock_place_order
     trade.content = '{"success":1,"return":{"receive_btc":0,"remain_rp":1000000,"order_id":11560,"balance":{"idr":"8000000"}}}'
     order.content = '{"success": 1,"return": {"order": {"order_id": "11560","price": "10000","type": "buy","order_idr": "1.2","remain_idr": "1.2", "submit_time": "'+ submit_time +'","finish_time": "0","status": "open"}}}'
-    expected = VipOrder(exchange, order_id, pair, 'buy', price, int(submit_time), amount)
+    expected = VipOrder(exchange, order_id, VipExchangeAccount.PAIRS[pair], 'buy', price, int(submit_time), amount)
     actual_test = exchange.place_buy_order(currency_pair=pair, price=price, amount=amount)
     assert actual_test == expected
     arg = VipExchangeAccount.BASE_URL
     data = OrderedDict([
         ('nonce', str(int(datetime.now().timestamp()))),
         ('method', 'trade'),
-        ('pair', pair),
+        ('pair', VipExchangeAccount.PAIRS[pair]),
         ('type', 'buy'),
         ('price', str(price)),
         ('btc', str(amount))
@@ -111,8 +120,18 @@ def test_place_buy_order(exchange, req_mock_place_order):
     assert req.call_args_list[0][0][0] == arg
     assert req.call_args_list[0][1]['data'] == data
 
+def test_place_buy_order_invalid_currency(exchange, req_mock_place_order):
+    pair = 'usdidr'
+    order_id = '11560'
+    price = 10000
+    amount = 1.2
+    submit_time = str(int(datetime.now().timestamp()))
+    with pytest.raises(RuntimeError) as excinfo:
+        exchange.place_buy_order(currency_pair=pair, price=price, amount=amount)
+    assert 'Invalid currency pair: ' + pair in str(excinfo.value)
+
 def test_place_sell_order(exchange, req_mock_place_order):
-    pair = 'btc_idr'
+    pair = BTCIDR
     order_id = '11560'
     price = 10000
     amount = 1.2
@@ -120,17 +139,27 @@ def test_place_sell_order(exchange, req_mock_place_order):
     req, trade, order = req_mock_place_order
     trade.content = '{"success":1,"return":{"receive_btc":0,"remain_rp":1000000,"order_id":11560,"balance":{"idr":"8000000"}}}'
     order.content = '{"success": 1,"return": {"order": {"order_id": "11560","price": "10000","type": "sell","order_btc": "1.2","remain_btc": "1.2", "submit_time": "'+ submit_time +'","finish_time": "0","status": "open"}}}'
-    expected = VipOrder(exchange, order_id, pair, 'sell', price, int(submit_time), amount)
+    expected = VipOrder(exchange, order_id, VipExchangeAccount.PAIRS[pair], 'sell', price, int(submit_time), amount)
     actual_test = exchange.place_sell_order(currency_pair=pair, price=price, amount=amount)
     assert actual_test == expected
     arg = VipExchangeAccount.BASE_URL
     data = OrderedDict([
         ('nonce', str(int(datetime.now().timestamp()))),
         ('method', 'trade'),
-        ('pair', pair),
+        ('pair', VipExchangeAccount.PAIRS[pair]),
         ('type', 'sell'),
         ('price', str(price)),
         ('idr', str(amount))
     ])
     assert req.call_args_list[0][0][0] == arg
     assert req.call_args_list[0][1]['data'] == data
+
+def test_place_sell_order_invalid_currency(exchange, req_mock_place_order):
+    pair = 'usdidr'
+    order_id = '11560'
+    price = 10000
+    amount = 1.2
+    submit_time = str(int(datetime.now().timestamp()))
+    with pytest.raises(RuntimeError) as excinfo:
+        exchange.place_sell_order(currency_pair=pair, price=price, amount=amount)
+    assert 'Invalid currency pair: ' + pair in str(excinfo.value)
