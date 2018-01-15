1cf8bb5d8a9cff40344b593c91dcd7516e0f55bd
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-01-15 20:22:14 +0700

    add driver program: app.py

--- app.py
+++ app.py
@@ -1,3 +1,45 @@
-from datetime import datetime
+import os
+import time
+import traceback
+from datetime import datetime, timedelta
+from dotenv import find_dotenv, load_dotenv
+from src.log import logger
+from src.exchange import VipExchangeAccount
+from src.market import BitcoinIndonesiaMarket
+from src.trader import Trader
+from src.trader.indicator import EMACrossoverIndicator
 
-print('Trading Bot initialized at ', datetime.now().strftime('%A, %d %B %Y. %X'))
+def initialize():
+    load_dotenv(find_dotenv())
+    vip_api_key = os.environ.get('vip_api_key')
+    vip_api_secret = os.environ.get('vip_api_secret')
+    vip_trades_symbol = os.environ.get('vip_trades_symbol')
+    initial_investment = os.environ.get('initial_investment')
+    exchange_account = VipExchangeAccount(vip_api_key, vip_api_secret)
+    market = BitcoinIndonesiaMarket()
+    ema_crossorver_35_50_10 = EMACrossoverIndicator(market.get_ohlc(
+        vip_trades_symbol, int((datetime.now() - timedelta(minutes=15*60)).timestamp()), period=15
+    ), 35, 50, confirm_period=10)
+    trader = Trader(vip_trades_symbol, exchange_account, market, ema_crossorver_35_50_10, 
+                    initial_investment, price_period=15)
+    begin_tick(trader)
+
+def begin_tick(trader):
+    while True:
+        try:
+            trader.tick()
+            time.sleep(15*60)
+        except Exception as ex:
+            end_tick(ex)
+
+def end_tick(error):
+    print(error)
+    print('Details:')
+    traceback.print_exc()
+    print()
+    print('Trading bot is stopped due to failures at', logger.time())
+    while True:
+        pass
+
+print('Trading Bot initialized at ', logger.time())
+initialize()
