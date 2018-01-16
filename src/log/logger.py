a374664af432e545a1efdd325e4edefe4fa63f46
Yudhistira Erlandinata <yudhistira.erlandinata@ui.ac.id>
2018-01-16 11:08:33 +0700

    add more log function in logger

--- src/log/logger.py
+++ src/log/logger.py
@@ -4,8 +4,24 @@ def time():
     utc7 = timezone(timedelta(hours=7), name='UTC+7')
     return datetime.now(utc7).strftime('%d %m %Y - %H.%M')
 
+def log_indicator_signal(is_buy, is_sell):
+    if is_buy:
+        print('[{}] buy signal detected'.format(time()))
+    elif is_sell:
+        print('[{}] sell signal detected'.format(time()))
+
+def log_order_issuing(order):
+    print('[{}] issuing {} order, id: {}, currency pair: {}, amount: {}, price: {}'
+          .format(time(), order.order_type, order.order_id,
+                  order.currency_pair, order.amount, order.price))
+
 def log_order_issue(order):
     print('[{}] {} order issued, id: {}, currency pair: {}, amount: {}, price: {}'
+          .format(time(), order.order_type, order.order_id,
+                  order.currency_pair, order.amount, order.price))
+
+def log_order_canceling(order):
+    print('[{}] canceling {} order, id: {}, currency pair: {}, amount: {}, price: {}'
           .format(time(), order.order_type, order.order_id,
                   order.currency_pair, order.amount, order.price))
 
